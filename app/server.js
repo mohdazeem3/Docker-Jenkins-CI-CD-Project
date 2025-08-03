b8e8f971bfa6f797b2d22f6429683d1b5a33dc3f
mohdazeem3 <mohamedazeem0170@gmail.com>
2025-08-03 12:43:32 +0530

    Create server.js

---
+++ app/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Docker + Jenkins App!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
